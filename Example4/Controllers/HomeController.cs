1601686abcb35894dd03d5d45984c9cd977b3edc
Prathyusha <prathyushapeddireddy@Prathyushas-MacBook-Air.local>
2021-12-18 15:59:53 -0500

    intial commit

---
+++ Example4/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Example4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost]
+        public JsonResult Calculate(DateTime time, int day) => new(new { Result = time.AddDays(day) });
+    }
+}
---
+++ Example4/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Example4.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public DateTime? Time { get; set; }
+
+        public TimeData TimeData { get; set; } = new TimeData();
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnGet()
+        {
+            TimeData.Day = 0;
+            TimeData.Time = DateTime.Now;
+            return Page();
+        }
+
+        public ActionResult OnPost(TimeData timeData)
+        {
+            Time = timeData.Time?.AddDays(timeData.Day);
+            return Page();
+        }
+    }
+
+    public class TimeData
+    {
+        public DateTime? Time { get; set; }
+        public int Day { get; set; }
+    }
+}
